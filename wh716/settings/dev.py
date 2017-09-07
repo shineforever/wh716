dd7edd2ed5e062babbff0ad0f241d3a9a280adbd
shineforever <shine_forever@yeah.net>
2017-09-07 17:33:46 +0800

    静态文件static和media支持七牛云oss

--- wh716/settings/dev.py
+++ wh716/settings/dev.py
@@ -2,5 +2,5 @@
 from wh716.settings.base import *
 
 
-QINIU_ACCESS_KEY = "-EtRvtpOvoRceczGnHsqvcd3W6c5hLhjs0uIP5T5"
-QINIU_SECRET_KEY = "Pqevq7miZZxW6yWDs0yrHKAm3Ef8-NCKt9qd1VqO"
+QINIU_ACCESS_KEY = ""
+QINIU_SECRET_KEY = ""
